17b6c30cfb2f805dc4b8b2b9bda13a975580ed98
ivankahnybediuk <56602081+ivankahnybediuk@users.noreply.github.com>
2020-04-30 12:58:28 +0300

    3

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-// React Template
+function Container (){
+    return <div className ="container">
+        <input className="screen" type="text"></input>
+        <div className="number-container">
+        <Button style="light-btn" text="AC"/>
+        <Button style="light-btn" text="+/-"/>
+        <Button style="light-btn" text="%"/>
+        <Button style="red-btn" text="/"/>
+        <Button style="btn" text="7"/>
+        <Button style="btn" text="8"/>
+        <Button style="btn" text="9"/>
+        <Button style="red-btn" text="*"/>
+        <Button style="btn" text="4"/>
+        <Button style="btn" text="5"/>
+        <Button style="btn" text="6"/>
+        <Button style="red-btn" text="-"/>
+        <Button style="btn" text="1"/>
+        <Button style="btn" text="2"/>
+        <Button style="btn" text="3"/>
+        <Button style="red-btn" text="+"/>
+        <Button style="big-btn" text="0"/>
+        <Button style="btn" text="."/>
+        <Button style="red-btn" text="="/>
+        </div>
+    </div>
+}
+function Button (props){
+return <div className="btn" className={props.style} >{props.text}</div>
+}
 
-// ================================================ JSX ==========================================================
-
-let myTitle = <h1>Hello React!</h1>;
-
-// Маємо скласти все в купу і можна виводити в HTML
-let page = <div>{myTitle}</div>;
+// ========================================================
+let page = <Container/>;
 
 // ================================================ Render ==========================================================
 
